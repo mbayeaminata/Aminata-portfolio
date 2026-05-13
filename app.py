bd6f04230308fe145ab345ac79043a2262f14929
mbayeaminata <aminatambaye777@gmail.com>
2026-05-13 10:04:22 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,54 @@
+import streamlit as st
+
+with st.sidebar:
+    
+
+
+    st.title("Contact")
+    st.write("- Email: aminatambaye777@gmail.com")
+    st.write("- Adresse: Saint-louis,Sénégal")
+
+    st.write("_____________")
+
+
+    
+    
+    
+    
+    
+   st.markdown(""" Merci d'avoir consulté mon cv. N'hésitez pas à me consulter pour toute collaboration, stage ou opportunité professionnelle.""")
+
+
+st.title("Aminata Mbaye") 
+
+
+st.header("Technicienne supérieure en Sciences Agricoles et Alimentaires")
+st.write("___________")
+
+st.subheader("A propos de moi")
+
+
+st.markdown("En tant que technicienne supérieure en agronomie, je suis passionnée par le développement agricole et l’innovation dans le secteur rural. Sérieuse, motivée et dynamique, j’ai des compétences en suivi des cultures, travaux de terrain et gestion agricole. J’aime travailler en équipe et apprendre de nouvelles techniques afin de contribuer à l’amélioration de l’agriculture au Sénégal et en Afrique.")
+
+
+
+st.title("Education")
+st.write("- 2024-2027 : Licence en Sciences Agricoles et Alimentaires")
+st.write("Université Amadou Makhtar Mbow de Diamnadio,Sénégal")
+st.write("- 2023-2024 :Baccalauréat Scientifique")
+st.write("Lycée Charles Degaulle,Saint-louis")
+
+
+st.title("Compétences techniques")
+st.write("- connaissances de base en production agricole") 
+st.write("- Compréhension des sols et de leur fertilité")
+st.write("- Analyse et interprétation de données simples")
+st.write("Utilisation des logiciels de base(Word,Excel,Powerpoint)")
+st.write("utilisation de Python(niveau débutant)")
+
+st.title("Expériences")
+st.write("- Participation au suivi et à l'entretien des cultures")
+st.write("- Collecte et analyse de données sur le terrain")
+st.write("- Sorties pédagogiques et visites techniques")
+st.write("- Utilisation et entretien de matériels agricoles")
+
